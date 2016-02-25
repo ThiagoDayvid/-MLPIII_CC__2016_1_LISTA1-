deef2a536ea52bd2129c08ff53290cc922937071
ThiagoDayvid <dayvid.contato@gmail.com>
2016-02-25 00:23:22 -0300

    Added files via upload
    exercicio referente ao 3 exercicio

---
+++ TesteCarro.java
@@ -0,0 +1,15 @@
+package Exercicios;
+
+public class TesteCarro {
+		public static void main(String[]args){
+		
+			Carro carro = new Carro();
+			
+			carro.ligar();
+			carro.acelerar(0.0);
+		
+		
+		
+		
+	}
+}
